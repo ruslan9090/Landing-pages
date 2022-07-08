a321d5306e2f4df34e6c3490fb2a522abc50b45d
Ruslan <style-26@mail.ru>
2022-07-08 12:20:56 +0400

    Update script.js

--- Pepsi/js/script.js
+++ Pepsi/js/script.js
@@ -1,7 +1,15 @@
 function imgSlider(anything) {
     document.querySelector('.pepsi').src = anything;
 }
+
 function changeBgColor(color){
-    const sec = document.querySelector('.sec');
-    sec.style.background = color;
+    const section = document.querySelector('.section');
+    section.style.background = color;
+}
+
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
